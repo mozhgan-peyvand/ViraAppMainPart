502d2a6dd76e604df66401e8106e20a696d74441
reza.hormozi <hormozi71@Gmail.com>
2023-08-23 09:40:11 +0330

    Implement force update changes (#42)

--- app/src/main/kotlin/ir/part/app/intelligentassistant/ui/screen/update/ForceUpdateScreen.kt
+++ app/src/main/kotlin/ir/part/app/intelligentassistant/ui/screen/update/ForceUpdateScreen.kt
@@ -2,7 +2,9 @@ package ir.part.app.intelligentassistant.ui.screen.update
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -21,6 +23,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import ir.part.app.intelligentassistant.R
+import ir.part.app.intelligentassistant.ui.theme.Color_Card
+import ir.part.app.intelligentassistant.ui.theme.Color_Text_1
+import ir.part.app.intelligentassistant.ui.theme.Color_White
 import ir.part.app.intelligentassistant.ui.theme.IntelligentAssistantTheme
 
 @Composable
@@ -29,38 +34,48 @@ fun ForceUpdateScreen(
     modifier: Modifier = Modifier
 ) {
     Column(
+        verticalArrangement = Arrangement.spacedBy(27.dp),
         modifier = modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colors.surface),
+            .background(Color_Card),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        Text(
+            modifier = Modifier.padding(top = 16.dp),
+            style = MaterialTheme.typography.subtitle1,
+            color = Color_White,
+            text = stringResource(id = R.string.lbl_update)
+        )
 
         Image(
             contentScale = ContentScale.FillWidth,
             modifier = Modifier
-                .size(200.dp)
-                .padding(start = 16.dp, end = 16.dp, top = 32.dp),
+                .size(200.dp),
             painter = painterResource(id = R.drawable.img_update),
             contentDescription = null
         )
 
+
         Text(
-            modifier = Modifier.padding(16.dp),
-            text = stringResource(id = R.string.lbl_update)
-        )
-        Text(
-            modifier = Modifier.padding(top = 32.dp, start = 16.dp, end = 16.dp, bottom = 16.dp),
+            modifier = Modifier.padding(horizontal = 33.dp),
+            style = MaterialTheme.typography.body2,
+            color = Color_Text_1,
             text = stringResource(id = R.string.msg_update)
         )
 
-
         Button(
+            contentPadding = PaddingValues(16.dp),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, bottom = 32.dp),
+                .padding(start = 16.dp, end = 16.dp, bottom = 38.dp),
             onClick = onUpdateClick
         ) {
-            Text(text = stringResource(id = R.string.lbl_update_app))
+            Text(
+                text = stringResource(id = R.string.lbl_update_app),
+                style = MaterialTheme.typography.button,
+                color = Color_White
+            )
         }
     }
 }
