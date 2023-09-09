c7422d9c5d4414c7536807dc0e03fcc29c22167a
reza.hormozi <hormozi71@Gmail.com>
2023-09-09 10:02:57 +0330

    implement about us screen (#63)

---
+++ app/src/main/kotlin/ir/part/app/intelligentassistant/features/home/about_us/AboutUsScreen.kt
@@ -0,0 +1,135 @@
+package ir.part.app.intelligentassistant.features.home.about_us
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import ir.part.app.intelligentassistant.R
+import ir.part.app.intelligentassistant.utils.ui.theme.Color_Primary_200
+import ir.part.app.intelligentassistant.utils.ui.theme.Color_Text_2
+import ir.part.app.intelligentassistant.utils.ui.theme.Color_White
+import ir.part.app.intelligentassistant.utils.ui.theme.IntelligentAssistantTheme
+
+@Composable
+fun AboutUsScreen(
+    navController: NavController
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painter = painterResource(id = R.drawable.bg_pattern),
+                contentScale = ContentScale.Crop
+            )
+
+    ) {
+        AboutUsTopAppBar(
+            onBackClick = {navController.navigateUp()},
+        )
+
+        Spacer(modifier = Modifier.size(32.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.img_about_us),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(height = 160.dp, width = 210.dp)
+                .align(CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.size(22.dp))
+
+        Text(
+            text = stringResource(id = R.string.app_name_farsi),
+            color = Color_Primary_200,
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Text(
+            text = stringResource(id = R.string.lbl_assistant),
+            color = Color_Primary_200,
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Text(
+            text = stringResource(id = R.string.desc_about_us),
+            color = Color_Text_2,
+            style = MaterialTheme.typography.body1,
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp, end = 16.dp, bottom = 18.dp)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+
+@Composable
+fun AboutUsTopAppBar(
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+    ) {
+        IconButton(onClick = onBackClick) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_arrow_forward),
+                contentDescription = null,
+                modifier = Modifier.padding(12.dp),
+                tint = Color_White
+            )
+        }
+
+        Spacer(modifier = Modifier.size(8.dp))
+
+        Text(
+            text = stringResource(id = R.string.lbl_about_us),
+            color = Color_White
+        )
+    }
+}
+
+
+@Preview
+@Composable
+private fun ArchiveEmptyBodyPreview() {
+    IntelligentAssistantTheme {
+        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+            AboutUsScreen(rememberNavController())
+        }
+    }
+}
