741731a7b3982acc2143371e2c290cb8500751bb
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-11-27 14:36:24 +0330

    Improve voice record screen (#200)
    - Show convertToText while paused
    - Change RecordingAnimation icon while recording
    - Remove select name bottom sheet
    - Change repeat icon
    - Enable convertToText icon while paused

--- app/src/main/kotlin/ai/ivira/app/features/ava_negar/ui/record/sheets/VoiceRecordingBottomSheetType.kt
+++ app/src/main/kotlin/ai/ivira/app/features/ava_negar/ui/record/sheets/VoiceRecordingBottomSheetType.kt
@@ -3,6 +3,5 @@ package ai.ivira.app.features.ava_negar.ui.record.sheets
 enum class VoiceRecordingBottomSheetType {
     BackConfirm,
     StartAgainConfirmation,
-    ConvertToTextConfirmation,
     MicrophoneIsBeingUsedAlready
 }
--- app/src/main/kotlin/ai/ivira/app/features/ava_negar/ui/record/widgets/RecordingAnimation.kt
+++ app/src/main/kotlin/ai/ivira/app/features/ava_negar/ui/record/widgets/RecordingAnimation.kt
@@ -3,16 +3,22 @@ package ai.ivira.app.features.ava_negar.ui.record.widgets
 import ai.ivira.app.R
 import ai.ivira.app.utils.ui.safeClickable
 import ai.ivira.app.utils.ui.widgets.ViraImage
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ContentTransform
 import androidx.compose.animation.core.AnimationVector2D
 import androidx.compose.animation.core.EaseOut
 import androidx.compose.animation.core.TwoWayConverter
 import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
 import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -26,7 +32,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -35,6 +43,7 @@ fun RecordingAnimation(
     onRecordClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
     var scale by remember(isRecording) { mutableFloatStateOf(0.7f) }
     var angle by remember(isRecording) {
         mutableFloatStateOf(0f)
@@ -77,6 +86,15 @@ fun RecordingAnimation(
         contentAlignment = Alignment.Center,
         modifier = modifier
             .clip(CircleShape)
+            .then(
+                if (isRecording) {
+                    Modifier
+                } else {
+                    Modifier.semantics {
+                        contentDescription = context.getString(R.string.desc_start_recording)
+                    }
+                }
+            )
             .safeClickable(
                 interactionSource = remember { MutableInteractionSource() },
                 indication = null,
@@ -113,16 +131,45 @@ fun RecordingAnimation(
             )
         }
 
-        ViraImage(
-            drawable = R.drawable.img_recording,
-            contentDescription = if (isRecording) {
-                null
-            } else {
-                stringResource(R.string.desc_start_recording)
-            },
-            modifier = Modifier
-                .size(89.dp)
-        )
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.size(89.dp)
+        ) {
+            ViraImage(
+                drawable = R.drawable.ic_recording_circle,
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize()
+            )
+            AnimatedContent(
+                targetState = isRecording,
+                label = "recordingIcon",
+                contentAlignment = Alignment.Center,
+                transitionSpec = {
+                    ContentTransform(
+                        targetContentEnter = fadeIn(tween(200))
+                            .plus(slideInVertically(tween(200), initialOffsetY = { it / 3 })),
+                        initialContentExit = slideOutVertically(
+                            tween(400),
+                            targetOffsetY = { it / 3 * 2 })
+                            .plus(scaleOut(tween(200)))
+                    )
+                }
+            ) { isRecording ->
+                if (isRecording) {
+                    ViraImage(
+                        drawable = R.drawable.ic_pause_2,
+                        contentDescription = null,
+                        modifier = Modifier.size(32.dp)
+                    )
+                } else {
+                    ViraImage(
+                        drawable = R.drawable.ic_mic_2,
+                        contentDescription = null,
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
+            }
+        }
     }
 }
 
