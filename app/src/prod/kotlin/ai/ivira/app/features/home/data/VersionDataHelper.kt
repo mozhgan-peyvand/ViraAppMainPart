4f09b3a269ba45a4cbab51cf7afa8f828b59703f
reza.hormozi <hormozi71@Gmail.com>
2023-12-03 09:16:03 +0330

    Add barjavand prod (#203)
    * Add barjavand prod
    * Encrypt barjavand url and authentication details

---
+++ app/src/prod/kotlin/ai/ivira/app/features/home/data/VersionDataHelper.kt
@@ -0,0 +1,26 @@
+package ai.ivira.app.features.home.data
+
+import javax.inject.Inject
+
+class VersionDataHelper @Inject constructor() {
+
+    init {
+        System.loadLibrary("vira")
+    }
+
+    fun gwu(): String = cgwu()
+
+    fun gw(): String = cgw()
+
+    fun up(): String = cup()
+
+    fun gwp(): String = cgwp()
+
+    fun gws(): String = cgws()
+
+    private external fun cgwu(): String
+    private external fun cgw(): String
+    private external fun cup(): String
+    private external fun cgwp(): String
+    private external fun cgws(): String
+}
