262c20a29f05ff151af7fda783fee9076ce56f00
Mohsen Sameti <s.mn.sameti@gmail.com>
2023-12-05 10:40:21 +0330

    Improve notification (#209)

--- app/src/main/kotlin/ai/ivira/app/utils/common/notification/ViraFirebaseMessagingService.kt
+++ app/src/main/kotlin/ai/ivira/app/utils/common/notification/ViraFirebaseMessagingService.kt
@@ -8,10 +8,15 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Typeface
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
+import android.text.Spannable
+import android.text.style.StyleSpan
 import androidx.core.app.NotificationCompat
+import androidx.core.text.buildSpannedString
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import timber.log.Timber
@@ -43,16 +48,34 @@ class ViraFirebaseMessagingService : FirebaseMessagingService() {
             PendingIntent.FLAG_IMMUTABLE
         )
 
+        val titleBold = buildSpannedString {
+            append(title)
+            if (title.isNotEmpty()) {
+                setSpan(
+                    StyleSpan(Typeface.BOLD),
+                    0,
+                    title.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+        }
+
         val defaultSoundUri =
             RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder =
-            NotificationCompat.Builder(this, NEW_NOTIFICATION_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_vira)
-                .setContentTitle(title)
-                .setContentText(messageBody)
-                .setAutoCancel(true)
-                .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent)
+        val notificationBuilder = NotificationCompat
+            .Builder(this, NEW_NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.img_vira_notification)
+            .setContentTitle(titleBold)
+            .setContentText(messageBody)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .setBigContentTitle(titleBold)
+                    .bigText(messageBody)
+            )
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.img_vira_notification))
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
