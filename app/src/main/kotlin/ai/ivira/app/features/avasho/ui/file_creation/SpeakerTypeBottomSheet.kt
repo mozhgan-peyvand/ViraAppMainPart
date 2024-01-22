b4a5ec1773b4be34676afcca5b91363464428e92
Amir Hosein Khooei <amirhossein.khouei@partsoftware.com>
2024-01-22 15:37:26 +0330

    Update speaker type config in Avasho (#299)
    order of speakers changed.
    
    woman speaker value changed.

--- app/src/main/kotlin/ai/ivira/app/features/avasho/ui/file_creation/SpeakerTypeBottomSheet.kt
+++ app/src/main/kotlin/ai/ivira/app/features/avasho/ui/file_creation/SpeakerTypeBottomSheet.kt
@@ -4,5 +4,5 @@ import ai.ivira.app.R
 
 enum class SpeakerTypeBottomSheet(val type: Int, val value: String) {
     MAN(type = R.string.lbl_man_speaker, "2"),
-    WOMAN(type = R.string.lbl_woman_speaker, "1")
+    WOMAN(type = R.string.lbl_woman_speaker, "3")
 }
